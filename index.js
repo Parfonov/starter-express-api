9c7fb3c02197bb4111d52cbe9b2621533bd0e786
Parfonov <Sergey.Parfonov@gmail.com>
2023-05-29 08:58:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.all('/', (req, res) => {
     res.send('Yo!')
 });
 app.post('/gptRequest', (req, res) => {
-	res.status(200).send(req.body.gptQuestion);
+    req.body.gptAnswer = "Проверка.";
+    res.status(200).send(req.body.gptQuestion);
 });
 app.listen(process.env.PORT || 3000)
