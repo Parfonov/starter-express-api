bffd45c41c72cf3c411fdee1f941ffc721a7005c
Parfonov <Sergey.Parfonov@gmail.com>
2023-05-29 09:03:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
+let chatGPT = require('./reqGPT');
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 });
 app.post('/gptRequest', (req, res) => {
-    res.status(200).send(req.body.gptQuestion);
+    res.status(200).send(chatGPT.getAnswer("Привет!");
 });
 app.listen(process.env.PORT || 3000)
