03ffcd6f2a76cde040e63a43d47d1352321b3039
Parfonov <Sergey.Parfonov@gmail.com>
2023-05-29 08:18:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ const express = require('express')
 const app = express()
 let gpt = require('./reqGPT');
 
-app.post('/mix/gpt', (req, res) => {
-    req.body.returnCode = "0";
-    req.body.returnMessage = "OK";
-    req.body.dateTime = getDateTimeFormat();
-    req.body.gptResponce = gpt.getAnswer(req.body.gptAsk);
-    res.status(200).send(req.body);
-});
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
